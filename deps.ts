a06fc12d84bbc076cff256728365f925682ee19e
Benjamin Pollack <benjamin@bitquabit.com>
2022-03-10 14:52:03 -0500

    update dependencies and Deno version

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-import * as flags from "https://deno.land/std@0.118.0/flags/mod.ts";
-import * as path from "https://deno.land/std@0.118.0/path/mod.ts";
-import * as toml from "https://deno.land/std@0.118.0/encoding/toml.ts";
+import * as flags from "https://deno.land/std@0.129.0/flags/mod.ts";
+import * as path from "https://deno.land/std@0.129.0/path/mod.ts";
+import * as toml from "https://deno.land/std@0.129.0/encoding/toml.ts";
 import {
   chunk,
   partition,
-} from "https://deno.land/std@0.118.0/collections/mod.ts";
-import { DateTime } from "https://esm.sh/luxon@2.2.0";
+} from "https://deno.land/std@0.129.0/collections/mod.ts";
+import { DateTime } from "https://esm.sh/luxon@2.3.1";
 import parseDate from "https://esm.sh/date.js@0.3.3";
 
 export { chunk, DateTime, flags, parseDate, partition, path, toml };
--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.118.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.129.0/testing/asserts.ts";
 
 import { makeEntry, parseEntries, renderEntry } from "./mod.ts";
 
