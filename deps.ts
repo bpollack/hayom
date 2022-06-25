553c69d3134c912efc3344f1fa9320d13d153487
Benjamin Pollack <benjamin@bitquabit.com>
2022-06-25 11:27:22 -0400

    deps: bump Deno stdlib to 0.145
    In addition to mechnically going to the newest Deno stdlib, this also
    updates the collections/ inclusions to follow contemporary best
    practices, which avoid using the mod.ts.
    
    Fixes: https://todo.sr.ht/~bmp/hayom/10

--- deps.ts
+++ deps.ts
@@ -1,10 +1,8 @@
-import * as flags from "https://deno.land/std@0.144.0/flags/mod.ts";
-import * as path from "https://deno.land/std@0.144.0/path/mod.ts";
-import * as toml from "https://deno.land/std@0.144.0/encoding/toml.ts";
-import {
-  chunk,
-  partition,
-} from "https://deno.land/std@0.144.0/collections/mod.ts";
+import * as flags from "https://deno.land/std@0.145.0/flags/mod.ts";
+import * as path from "https://deno.land/std@0.145.0/path/mod.ts";
+import * as toml from "https://deno.land/std@0.145.0/encoding/toml.ts";
+import { chunk } from "https://deno.land/std@0.145.0/collections/chunk.ts";
+import { partition } from "https://deno.land/std@0.145.0/collections/partition.ts";
 import { DateTime } from "https://esm.sh/luxon@~2.3.2";
 import parseDate from "https://esm.sh/date.js@0.3.3";
 
