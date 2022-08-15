8b7e1e8a9296247ab62dec8dae707951018d8d57
Benjamin Pollack <benjamin@bitquabit.com>
2022-08-15 09:37:01 -0400

    upgrade Deno stdlib

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-import * as flags from "https://deno.land/std@0.145.0/flags/mod.ts";
-import * as path from "https://deno.land/std@0.145.0/path/mod.ts";
-import * as toml from "https://deno.land/std@0.145.0/encoding/toml.ts";
-import { chunk } from "https://deno.land/std@0.145.0/collections/chunk.ts";
-import { partition } from "https://deno.land/std@0.145.0/collections/partition.ts";
+import * as flags from "https://deno.land/std@0.152.0/flags/mod.ts";
+import * as path from "https://deno.land/std@0.152.0/path/mod.ts";
+import * as toml from "https://deno.land/std@0.152.0/encoding/toml.ts";
+import { chunk } from "https://deno.land/std@0.152.0/collections/chunk.ts";
 import { DateTime } from "https://esm.sh/luxon@~2.3.2";
+import { partition } from "https://deno.land/std@0.152.0/collections/partition.ts";
 import parseDate from "https://esm.sh/date.js@0.3.3";
 
 export { chunk, DateTime, flags, parseDate, partition, path, toml };
