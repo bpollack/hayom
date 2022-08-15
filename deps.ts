4303bcf688b1757984a20fcf4d2675b8e1b20af3
Benjamin Pollack <benjamin@bitquabit.com>
2022-08-15 09:40:11 -0400

    upgrade luxon

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ import * as flags from "https://deno.land/std@0.152.0/flags/mod.ts";
 import * as path from "https://deno.land/std@0.152.0/path/mod.ts";
 import * as toml from "https://deno.land/std@0.152.0/encoding/toml.ts";
 import { chunk } from "https://deno.land/std@0.152.0/collections/chunk.ts";
-import { DateTime } from "https://esm.sh/luxon@~2.3.2";
+import { DateTime } from "https://esm.sh/luxon@~3.0.1";
 import { partition } from "https://deno.land/std@0.152.0/collections/partition.ts";
 import parseDate from "https://esm.sh/date.js@0.3.3";
 
