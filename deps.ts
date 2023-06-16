43c77023ed628025b32ccd24bb0816d59901bc93
Benjamin Pollack <benjamin@bitquabit.com>
2023-06-15 22:04:30 -0400

    fix the build and deps

--- deps.ts
+++ deps.ts
@@ -2,8 +2,9 @@ import * as flags from "https://deno.land/std@0.191.0/flags/mod.ts";
 import * as path from "https://deno.land/std@0.191.0/path/mod.ts";
 import * as toml from "https://deno.land/std@0.191.0/toml/mod.ts";
 import { chunk } from "https://deno.land/std@0.191.0/collections/chunk.ts";
-import { DateTime } from "https://esm.sh/luxon@~3.2.1";
 import { partition } from "https://deno.land/std@0.191.0/collections/partition.ts";
+
+import { DateTime } from "https://esm.sh/luxon@~3.3.0";
 import parseDate from "https://esm.sh/date.js@0.3.3";
 
 export { chunk, DateTime, flags, parseDate, partition, path, toml };
